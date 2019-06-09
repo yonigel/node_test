3fc969ead4facb3ab574b57d318102837a0111f3
unknown <yonatan@taranis.ag>
2019-06-09 16:29:22 +0300

    add only if not exists

--- src/controllers/db/relatedItemsController.js
+++ src/controllers/db/relatedItemsController.js
@@ -4,16 +4,29 @@ const getRelatedItems = (req, res) => {
 
 }
 
-const setRelatedItems = (req, res) => {
-    if (!req.body.itemId || !req.body.itemStore || !req.body.relatedIds) {
+const setRelatedItems = async (req, res) => {
+    if (!req.body.itemId || !req.body.itemStore || !req.body.relatedItems) {
         throw new Error('some of params are missing')
     }
 
+    // TODO - first check if item id and store not exist yet
+    // TODO - if exists - do nothing
+    let isExists;
+
+    const existRelatedItems = await RelatedItemsSchema.find({itemId: req.body.itemId, itemStore: req.body.itemStore}).exec();
+    isExists = existRelatedItems.length > 0
+
+    if (isExists) {
+        res.send('related item already exists in db');
+        return;
+    }
+    
+    
     let relatedItem = new RelatedItemsSchema(
         {
             itemId: req.body.itemId,
             itemStore: req.body.itemStore,
-            relatedItems: req.body.relatedIds
+            relatedItems: req.body.relatedItems
         }
     );
 
